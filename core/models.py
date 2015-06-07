52097d305654fbde8e0bf10fce2cae0fb7fe55c6
Josh Cai <yuanshi419@gmail.com>
2015-06-07 02:09:13 -0700

    add UserInfo data model and remove migrations folders from repo

--- core/models.py
+++ core/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class UserInfo(models.Model):
+  user = models.OneToOneField(User)
+  utd_id = models.CharField(max_length=50)
+  major = models.CharField(max_length=200)
+  major2 = models.CharField(max_length=200)
+  minor = models.CharField(max_length=200)
+  minor2 = models.CharField(max_length=200)
+  hometown = models.CharField(max_length=200)
+  high_school = models.CharField(max_length=200)
+  phone_number = models.CharField(max_length=200)
+  #TODO: add address field
+  #TODO: allow multiple phone
+  #TODO: allow backup emails
