d899146063861705134cbf454a43136c44ef1d25
Josh Cai <yuanshi419@gmail.com>
2015-06-06 21:45:13 -0700

    fix Jinja2 with django admin, add jinja2 as dependency requirement

--- mcdermott/settings.py
+++ mcdermott/settings.py
@@ -69,6 +69,33 @@ TEMPLATES = [
   },
 ]
 
+TEMPLATES = [
+  {
+    'BACKEND': 'django.template.backends.jinja2.Jinja2',
+    'DIRS': [],
+    'APP_DIRS': True,
+    'OPTIONS': {
+      'environment': 'mcdermott.jinja2_config.environment',
+    },
+  },
+  {
+    'BACKEND': 'django.template.backends.django.DjangoTemplates',
+    'DIRS': [],
+    'APP_DIRS': True,
+    'OPTIONS': {
+      'context_processors': [
+        'django.contrib.auth.context_processors.auth',
+        'django.template.context_processors.debug',
+        'django.template.context_processors.i18n',
+        'django.template.context_processors.media',
+        'django.template.context_processors.static',
+        'django.template.context_processors.tz',
+        'django.contrib.messages.context_processors.messages',
+      ],
+    },
+  },
+]
+
 WSGI_APPLICATION = 'mcdermott.wsgi.application'
 
 
