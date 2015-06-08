4ee350f6b22261246c3c509c455e2428a7400801
Hans Ajieren <hajieren@yahoo.com>
2015-06-07 20:05:30 -0500

    Created edit_info.html page where user will be able to edit his info

--- core/views.py
+++ core/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
   return render(request, 'core/index.html')
+
+def edit_info(request):
+	return render(request, 'core/edit_info.html')
