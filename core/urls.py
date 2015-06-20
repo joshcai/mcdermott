1c8fbeba14cfdc09eebb413f7955900b8cce2427
Josh Cai <yuanshi419@gmail.com>
2015-06-20 00:09:31 -0700

    add new scholars page

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
   url(r'^$', views.index, name='index'),
-  url(r'^profile/(\w+)/(\w+)$', views.profile, name='profile'),
   url(r'^edit_info$', views.edit_info, name='edit_info'),
+  url(r'^scholars$', views.scholars, name='scholars'),
+  url(r'^profile$', views.own_profile, name='own_profile'),
+  url(r'^(\w+)$', views.profile, name='profile'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,6 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.http import Http404
 from django.shortcuts import render, redirect
 
 from forms import McUserForm
@@ -27,9 +29,35 @@ def edit_info(request):
   return render(request, 'core/edit_info.html', context)
 
 @login_required
-def profile(request, first_name, last_name):
-  profile = McUser.objects.get(user__first_name=first_name, 
-                               user__last_name=last_name)
+def scholars(request):
+  scholars = User.objects.all()
+  context = {
+    'scholars': scholars
+    }
+  return render(request, 'core/scholars.html', context)
+
+
+def normalize_name(name):
+  return ''.join([c.lower() for c in name if c.isalpha()])
+
+@login_required
+def own_profile(request):
+  name = normalize_name(request.user.get_full_name())
+  return redirect('/%s' % name)
+
+@login_required
+def profile(request, name):
+  # TODO(joshcai): make this more efficient by caching or indexing all users
+  users = McUser.objects.all()
+  name = normalize_name(name)
+  profile = None
+  for user in users:
+    if normalize_name(user.user.get_full_name()) == name:
+      profile = user
+      break
+  # TODO(joshcai): handle case where more than 1 user (aka users have same name)
+  if profile is None:
+    raise Http404('Page does not exist')
   # 'user' is already passed in as default (the logged in user), 
   # so use 'profile' as alias 
   context = {
