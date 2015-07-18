6c9c0f8544c1017096f79d5a89f986a856f257d0
Josh Cai <yuanshi419@gmail.com>
2015-07-18 05:45:05 -0700

    fix tests

--- core/tests.py
+++ core/tests.py
@@ -7,15 +7,16 @@ class CoreTestCase(TestCase):
   def setUp(self):
     self.app = Client()
     self.user = User.objects.create_user('test', 'a@a.com', 'password')
-    self.user.first_name = 'Test'
-    self.user.last_name = 'Name'
     self.user.save()
+    self.user.mcuser.first_name = 'Test'
+    self.user.mcuser.last_name = 'Name'
+    self.user.mcuser.save()
 
   def login(self):
     self.app.login(username='test', password='password')    
 
   def testGetFullName(self):
-    self.assertEqual(self.user.get_full_name(), 'Test Name')
+    self.assertEqual(self.user.mcuser.get_full_name(), 'Test Name')
 
   def testMcUserGetsCreated(self):
     self.assertIsNotNone(self.user.mcuser)
@@ -29,7 +30,7 @@ class CoreTestCase(TestCase):
     self.login()
     response = self.app.get('/search?q=Test')
     self.assertIn('Test Name', response.content)
-    self.user.first_name = 'Foo'
-    self.user.save()
+    self.user.mcuser.first_name = 'Foo'
+    self.user.mcuser.save()
     response = self.app.get('/search?q=Foo')
     self.assertIn('Foo Name', response.content)
