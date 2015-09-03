c7970fddea3fedd9ecbdcf46620ce9e8f64cb085
atvaccaro <atvaccaro@gmail.com>
2015-09-03 14:28:23 -0500

    More calendar work; something wrong with url reversematch

--- mccalendar/urls.py
+++ mccalendar/urls.py
@@ -4,6 +4,10 @@ from . import views
 
 urlpatterns = [
   url(r'^$', views.index, name='index'),
-  url(r'^create_event$', views.create_event, name='create_event'),
-  url(r'^([0-9]+)/$', views.event_detail, name='event_detail'),
+  url(r'^create_event/$', views.create_event, name='create_event'),
+  url(r'^event/([0-9]+)/$', views.event_detail, name='event_detail'),
+  url(r'^([0-9]+)/([0-9]+)/(prev|next)/$', views.month, name='month'),
+  url(r'^([0-9]+)/([0-9]+)/$', views.month, name='month'),
+  url(r'^([0-9]+)/([0-9]+)/([0-9]+)/$', views.day, name='day'),
+
 ]
--- mccalendar/views.py
+++ mccalendar/views.py
@@ -4,19 +4,113 @@ from django.contrib.auth.models import User
 from django.http import Http404
 from django.shortcuts import render, redirect
 
+import time
+import calendar
+
 from models import McEvent
 from forms import McEventForm
 
 # Create your views here.
 
+month_names = "January February March April May June July August September October November December".split()
+
 #Default calendar view
 @login_required
 def index(request):
+  cur_year, cur_month = time.localtime()[:2]
   events = McEvent.objects.all()
   context = {
     'events': events,
   }
-  return render(request, 'mccalendar/index.html', context)
+  return redirect('/mccalendar/%s/%s/' % (cur_year, cur_month))
+
+"""
+@login_required
+def year(request, year=None):
+  cur_year, cur_month = time.localtime()[:2]
+
+  #create list of months
+  month_list = []
+  for nm, month in enumerate(months):
+    event = current = False
+    events = McEvent.objects.filter(start_date__year=year, start_date__month=n+1)
+
+    if events:
+      event = True
+    if year == cur_year and nm+1 == cur_month:  #enumerate starts at 0, but months at 1
+      current = True  #to highlight the current month?
+    month_list.append(dict(n=n+1, name=month, entry=entry, current=current))
+
+  context = {
+    'n':n,
+    'months':month_list,
+    'year':year,
+  }
+  return render(request, 'mccalendar/year.html', context)
+"""
+
+@login_required
+def month(request, year=None, month=None, change=None):
+  year = int(year) if year else time.localtime()[0]
+  month = int(month) if month else time.localtime()[1]
+
+  #apply next/previous if applicable
+  if change in ('next', 'prev'):
+    if change=='next':
+      month = month+1
+      if month == 13:
+        month = 1
+        year += 1
+    elif change=='prev':
+      month = month-1
+      if month == 0:
+        month = 12
+        year -= 1
+    return redirect('/mccalendar/%s/%s/' % (year, month))
+
+  cur_year, cur_month, cur_day = time.localtime()[:3]
+
+  cal = calendar.Calendar()
+  cal.setfirstweekday(calendar.SUNDAY)
+  month_days = cal.itermonthdays(year, month)
+
+  events = []
+  lst=[[]]
+  week = 0
+
+  for day in month_days:
+    entries = current = False
+    if day:
+      events = McEvent.objects.filter(start_date__year=year, start_date__month=month, start_date__day=day)
+      if day == cur_day and year == cur_year and month == cur_month:
+        current = True
+    lst[week].append((day, events, current))
+    if len(lst[week]) == 7:
+      lst.append([])
+      week += 1
+  context = {
+    'year': year,
+    'month': month,
+    'day': day,
+    'month_name': month_names[month-1],
+    'month_days': lst,
+  }
+  return render(request, 'mccalendar/month.html', context)
+
+@login_required
+def day(request, year=None, month=None, day=None):
+  year = int(year) if year else time.localtime()[0]
+  month = int(month) if month else time.localtime()[1]
+
+  events = McEvent.objects.filter(start_date__year=year, start_date__month=month, start_date__day=day)
+  context = {
+    'year': year,
+    'month': month,
+    'day': day,
+    'month_name': month_names[month-1],
+    'events': events,
+  }
+  return render(request, 'mccalendar/day.html', context)
 
 @login_required
 def create_event(request):
@@ -31,7 +125,7 @@ def create_event(request):
   else:
     form = McEventForm()
   context = {
-        'form':form,
+    'form':form,
   }
   return render(request, 'mccalendar/create_event.html', context)
 
