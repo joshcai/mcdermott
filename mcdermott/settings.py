67a22548ca554e5ba58026168b946f55baaad5a4
Josh Cai <yuanshi419@gmail.com>
2015-09-13 14:04:56 -0500

    add ability to use postgres

--- mcdermott/settings.py
+++ mcdermott/settings.py
@@ -36,6 +36,9 @@ if allowed_hosts_env:
   allowed_hosts_env = [x.strip() for x in allowed_hosts_env.split(',')]
 ALLOWED_HOSTS = allowed_hosts_env or config.ALLOWED_HOSTS
 
+use_postgres = os.environ.get('USE_POSTGRES') or config.USE_POSTGRES
+db_password = os.environ.get('DB_PASSWORD') or config.DB_PASSWORD
+
 
 # Application definition
 
@@ -96,12 +99,22 @@ WSGI_APPLICATION = 'mcdermott.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
+sqlite_settings = {
+  'ENGINE': 'django.db.backends.sqlite3',
+  'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+}
+
+postgres_settings = {
+  'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+  'NAME': 'mcdermott',                      # Or path to database file if using sqlite3.
+  'USER': 'postgres',
+  'PASSWORD': db_password,
+  'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
+  'PORT': '',                      # Set to empty string for default.
+}
 
 DATABASES = {
-  'default': {
-    'ENGINE': 'django.db.backends.sqlite3',
-    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-  }
+  'default': postgres_settings if use_postgres else sqlite_settings
 }
 
 # Caches
